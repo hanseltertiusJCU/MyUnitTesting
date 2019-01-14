b23f728c03ddbc54db12fe351d73687c6263408e
Hansel Tertius <hansel.tertius@my.jcu.edu.au>
2019-01-14 12:26:35 +0700

    Initial commit: Implement unit testing

---
+++ app/src/main/java/com/example/android/myunittesting/MainModel.java
@@ -0,0 +1,15 @@
+package com.example.android.myunittesting;
+
+public class MainModel {
+
+    private double volume;
+
+    public MainModel(double volume){
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+}
---
+++ app/src/main/java/com/example/android/myunittesting/MainView.java
@@ -0,0 +1,5 @@
+package com.example.android.myunittesting;
+
+public interface MainView {
+    void tampilVolume(MainModel model);
+}
